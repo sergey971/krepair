a178e34733aac03e12fada3f02aef3357342f8d0
Sergey <78379444+sergey971@users.noreply.github.com>
2022-09-10 23:20:46 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+    center:true,
+    loop: true,
+    lazyLoad: true,
+    margin: 30,
+    startPosition: 0,
+    items: 4,
+    lazyLoad: true,
+    responsive: {
+        425: {
+            items: 1,
+        },
+        720: {
+            items: 4,
+            margin: 10,
+        },
+        1024: {
+            items:4,
+            margin: 20,
+        },
+    },
+});
+
+$('.slider__btn--prev').click(function () {
+    owl.trigger('prev.owl.carousel');
+})
+
+$('.slider__btn--next').click(function () {
+    owl.trigger('next.owl.carousel');
+})
+
+
+
