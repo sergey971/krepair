1cb10f619671ef37b02b132997edb9a78a4a4db1
Sergey <murin.89@list.ru>
2022-12-10 23:19:03 +0300

    add class menu__list--active
    add class menu__list--active

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ $('.slider__btn--next').click(function () {
 })
 
 $('.menu__btn').on('click', function(){
-    $('menu__list').toggleClass('menu__list--active');
+    $('.menu__list').toggleClass('menu__list--active');
 });
 
 
